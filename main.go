9f28cb7a18b7707a0da392129680875f027dd416
ahhzaky <siahmadzaky@gmail.com>
2023-10-29 22:58:42 +0700

    TODOCOMMENT

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+// Route Api
+
+// User Reviewer
+// Create Review with User Review Token? Active Campaign
+
+// Update Review  with User Review Token? Active Campaign
+
+// Get All Review  with User Review Token? Active Campaign : Deactive Campaign : Report Campaign
+
+// // Delete Review ? Active Campaign
+
+// Admin Service Accsess
+// GET STATUS SERVICE WITH VERIFY TOKEN ADMIN SERVICE
+
+// GET LOG SERVICE WITH VERIFY TOKEN ADMIN SERVICE
+
+// Get All Review ? Active Campaign : Deactive Campaign : Report Campaign
+
+// Delete Review
